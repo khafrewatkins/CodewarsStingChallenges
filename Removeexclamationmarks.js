4f6547446ebf037b80ceffc85e48ef00ac1ac453
khafrewatkins <khafrewatkins@gmail.com>
2021-05-16 14:42:43 -0500

    Create Removeexclamationmarks.js

---
+++ Removeexclamationmarks.js
@@ -0,0 +1,10 @@
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+function removeExclamationMarks(s) {
+  let news = s.replace(/!/g, '')
+  return news
+}
+
+
+
